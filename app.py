716250ced1c6c81e046c786d539657053a2564bd
Rystambek <rustambekjumaqulov@gmail.com>
2023-02-26 19:07:14 +0500

    app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from db import GroceryDB
 
 
@@ -10,35 +10,126 @@ db = GroceryDB()
 @app.route('/grocery')
 def all_grocery():
     """Get all grocery"""
-    pass
+    fruits = db.all()
+    html = '<html>'
+    html += '<head><title>Fruit</title><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: left;}</style></head>'
+    html += '<body>'
+    html += '<h1>Fruits</h1>'
+    html += '<table>'
+    html += '<tr><th>name</th> <th>quantity</th> <th>price</th> <th>type</th></tr>'
+    for fruit in fruits:
+        html += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+    html += '</table>'
+    html += '</body>'
+    html += "</html>"
+    return html
 
 
 # view add grocery
 @app.route('/grocery/add', methods=['POST'])
 def add_grocery():
     """Add a grocery"""
-    pass
+    fruit = request.get_json(force=True)
+    fruits = db.add(fruit)
+    return fruits
 
 
 # view all grocery by type
 @app.route('/grocery/type/<type>')
 def all_grocery_by_type(type):
     """Get all grocery by type"""
-    pass
+    fruits = db.get_by_type(type)
+    if fruits :
+        html = """<html>
+        <head>
+        <title>Fruit</title>
+        <style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: left;}</style>
+        </head>
+        <body>
+        <h1>Fruits type</h1>
+        <table>
+        <tr><th>name</th> <th>quantity</th> <th>price</th> <th>type</th></tr>
+        """
+        for fruit in fruits:
+            html += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        html += """
+        </table>
+        </body>
+        </html>
+        """
 
+    else:
+        html = """<html>
+        <head><title>ERROR</title></head>
+        <body><h1> ERROR </h1></body>
+        </html>
+        """
+    return html
 
 # view all grocery by name
 @app.route('/grocery/name/<name>')
 def all_grocery_by_name(name):
     """Get all grocery by name"""
-    pass
+    fruits = db.get_by_name(name)
+    if fruits :
+        html = """
+        <html>
+        <head>
+        <title>Fruit</title>
+        <style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: left;}</style>
+        </head>
+        <body>
+        <h1>Fruits name</h1>
+        <table>
+        <tr><th>name</th> <th>quantity</th> <th>price</th> <th>type</th></tr>
+        """
+        for fruit in fruits:
+            html += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        html += """</table>
+        </body>
+        </html>
+        """
+
+    else:
+        html = """<html>
+        <head><title>ERROR</title></head>
+        <body><h1> ERROR </h1></body>
+        </html>
+        """
+    return html
 
 
 # view all grocery by price
 @app.route('/grocery/price/<float:price>')
 def all_grocery_by_price(price):
     """Get all grocery by price"""
-    pass
+    fruits = db.get_by_price(price)
+    if fruits :
+        html = """
+        <html>
+        <head>
+        <title>Fruit</title>
+        <style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: left;}</style>
+        </head>
+        <body>
+        <h1>Fruits price</h1>
+        <table>
+        <tr><th>name</th> <th>quantity</th> <th>price</th> <th>type</th></tr>
+        """
+        for fruit in fruits:
+            html += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        html += """</table>
+        </body>
+        </html>
+        """
+
+    else:
+        html = """<html>
+        <head><title>ERROR</title></head>
+        <body><h1> ERROR </h1></body>
+        </html>
+        """
+    return html
 
 
 
